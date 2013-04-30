b312e98b2381c9b7da38035ef108b298748f36a0
André Santos <andreccdr@gmail.com>
2013-04-30 17:12:07 +0100

    Cabeçalho exercicio 2

---
+++ EXERCICIO2_G10.pl
@@ -0,0 +1,26 @@
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SIST. REPR. CONHECIMENTO E RACIOCINIO - LEI/2013
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% TRABALHO DE GRUPO - EXERCÍCIO 2
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Sistema de representação de conhecimento e raciocínio com capacidade para caracterizar um universo de discurso de âmbito farmacêutico.
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Invariantes
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SICStus PROLOG: Declaracoes iniciais
+
+:- set_prolog_flag( discontiguous_warnings,off ).
+:- set_prolog_flag( single_var_warnings,off ).
+:- set_prolog_flag( unknown,fail ).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% SICStus PROLOG: definicoes iniciais
+
+:- op( 900,xfy,'::' ).
+
+
+
