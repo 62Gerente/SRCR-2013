cc39d53ba3eeeb693a8f80e3f0df134cca8a33f4
André Santos <andreccdr@gmail.com>
2013-04-30 20:07:23 +0100

    Dados teste ben-u-ron até armario

--- EXERCICIO2_G10.pl
+++ EXERCICIO2_G10.pl
@@ -23,6 +23,50 @@
 :- op( 900,xfy,'::' ).
 :- dynamic '-'/1.
 
+% Extensao do predicado medicamento: Nome -> {V,F,D}
+
+medicamento('ben-u-ron').
+
+% Extensao do predicado indicacoes_terapeuticas: Medicamento,Indicacao -> {V,F,D}
+
+indicacoes_terapeuticas('ben-u-ron','Sindromes gripais').
+indicacoes_terapeuticas('ben-u-ron','Enxaquecas').
+indicacoes_terapeuticas('ben-u-ron','Dores de dentes').
+
+-indicacoes_terapeuticas('ben-u-ron','Queimaduras').
+ 
+% Extensao do predicado principio_activo: Medicamento,Substancia -> {V,F,D}
+
+principio_activo('ben-u-ron','Paracetamol').
+
+-principio_activo(M,S) :- principio_activo(M,X).  % S=!X
+
+% Extensao do predicado apresentacao_farmaceutica: Medicamento,Apresentacao -> {V,F,D}
+
+apresentacao_farmaceutica('ben-u-ron','Comprimidos').
+apresentacao_farmaceutica('ben-u-ron','Xarope').
+
+-apresentacao_farmaceutica('ben-u-ron','Pensos').
+
+% Extensao do predicado aplicacao_clinica: Medicamento,Aplicacao -> {V,F,D}
+
+aplicacao_clinica(M,A) :- indicacoes_terapeuticas(M,A).
+
+aplicacao_clinica('ben-u-ron','Sonolencia').
+
+% Extensao do predicado armario: Nome,Apresentacao -> {V,F,D}
+
+armario('C A-G','Comprimidos').
+
+% Extensao do predicado prateleira: Nome, Armario -> {V,F,D}
+
+prateleira('C A-C','C A-G').
+prateleira('C D-F','C A-G').
+
+% Estensao do predicado esta_prateleira: Medicamento, Armario, Prateleira -> {V,F,D}
+
+local('ben-u-ron','C A-G','C A-C').
+
 % Extensao do meta-predicado demo: Questao,Resposta -> {V,F}
 
 demo( Questao,verdadeiro ) :-
